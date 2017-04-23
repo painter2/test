2c8a2ef630763624ef6f65166c03beaa0bd5c369
painter2 <t>
2017-04-23 10:41:34 +0900

    move

--- docs/test.js
+++ docs/test.js
@@ -1,1 +1,16 @@
 console.log('hello world')
+var test = document.getElementById('test');
+var log = document.getElementById('log')
+var count = 0;
+test.addEventListener('click', function () {
+  log.textContent = 'clicked' + count
+  count++;
+})
+
+test.addEventListener('touchend', function () {
+  log.textContent = 'touchend' + count
+  count++;
+})
+
+log.appendChild('screen.innerWidth ' + screen.innerWidth);
+log.appendChild('screen.innerHeight ' + screen.innerHeight);
